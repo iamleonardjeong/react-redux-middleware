59d52dce591c333cbcc7b379117cc04447994597
iamleonardjeong <iamleonardjeong@gmail.com>
2021-03-23 18:08:30 +0900

    redux-saga

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import CounterContainer from './containers/CounterContainer';
 import SampleContainer from './containers/SampleContainer';
 
 function App() {
-  return <SampleContainer />;
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
 }
 
 export default App;
-
-function* watchGenerator() {
-  console.log('모니터링 중...');
-  let prevAction = null;
-  while (true) {
-    const action = yield;
-    console.log('이전 액션: ', prevAction);
-    prevAction = action;
-    if (action.type === 'HELLO') {
-      console.log('안녕하세요');
-    }
-  }
-}
-
-const watch = watchGenerator();
